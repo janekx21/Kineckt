5aa140cab4ad05b6ff8f8baac8bc5fedc65978bd
Janek Winkler <janekx21@gmail.com>
2020-04-23 15:24:01 +0200

    working scene and rendering splitted

--- Kineckt/src/Kineckt.cs
+++ Kineckt/src/Kineckt.cs
@@ -5,26 +5,18 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Kineckt {
     public class Kineckt : Game {
+        public static Effect DefaultEffect { get; private set; } = null;
+        public static Effect DefaultShadowMapEffect { get; private set; } = null;
         private readonly GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private Model _model;
 
-        private const float NearClipPlane = .1f;
-
-        private const float FarClipPlane = 200;
-
-        private readonly float _fieldOfView = MathHelper.ToRadians(80f);
-
-        private Texture2D _texture;
-        private Effect _shadowMapGenerate;
         private RenderTarget2D _shadowMapRenderTarget;
-        private Effect _diffuse;
         private RenderTarget2D _mainBuffer;
+        
         private DepthStencilState _depth;
-        private Model _plane;
+        
         private Scene _scene;
-
-        private float AspectRatio => _graphics.PreferredBackBufferWidth / (float) _graphics.PreferredBackBufferHeight;
+        private ModelRenderer _ship;
 
         public Kineckt() {
             _graphics = new GraphicsDeviceManager(this) {GraphicsProfile = GraphicsProfile.HiDef};
@@ -36,25 +28,18 @@ namespace Kineckt {
 
             IsMouseVisible = true;
 
-            _shadowMapRenderTarget = new RenderTarget2D(GraphicsDevice,
-                2048, 2048, false, SurfaceFormat.Single,
-                DepthFormat.Depth24, 0, RenderTargetUsage.PlatformContents);
-            _depth = new DepthStencilState
-                {DepthBufferEnable = true, DepthBufferFunction = CompareFunction.Less};
+            SetupBuffers();
 
-            _mainBuffer = new RenderTarget2D(GraphicsDevice, GraphicsDevice.Viewport.Width,
-                GraphicsDevice.Viewport.Height, false, SurfaceFormat.Color,
-                DepthFormat.Depth24Stencil8, 0, RenderTargetUsage.DiscardContents);
-
-
-            Window.ClientSizeChanged += new EventHandler<EventArgs>((sender, args) => {
+            Window.ClientSizeChanged += (sender, args) => {
                 _mainBuffer = new RenderTarget2D(GraphicsDevice, GraphicsDevice.Viewport.Width,
                     GraphicsDevice.Viewport.Height, false, SurfaceFormat.Color,
                     DepthFormat.Depth24Stencil8, 0, RenderTargetUsage.DiscardContents);
-            });
+            };
 
             _scene = new Scene();
-            _scene.Spawn(new Sun());
+            _scene.Spawn(new Sun() {
+                Position = new Vector3(2, 8, 2)
+            });
             _scene.Spawn(new Camera(GraphicsDevice));
 
             base.Initialize();
@@ -62,18 +47,20 @@ namespace Kineckt {
 
         protected override void LoadContent() {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _model = Content.Load<Model>("models/starshipOmega");
-            _plane = Content.Load<Model>("models/plane");
-            _texture = Content.Load<Texture2D>("images/starshipOmegaPaintOver");
+            var model = Content.Load<Model>("models/starshipOmega");
+            var plane = Content.Load<Model>("models/plane");
+            var texture = Content.Load<Texture2D>("images/starshipOmegaPaintOver");
             var krakula = Content.Load<Texture2D>("images/krakula-xl");
-            _shadowMapGenerate = Content.Load<Effect>("shaders/ShadowMapsGenerate");
-            _diffuse = Content.Load<Effect>("shaders/Diffuse");
-            
-            _scene.Spawn(new ModelRenderer("foo", GraphicsDevice, _shadowMapRenderTarget) {
-                Model = _model, Effect = _diffuse, Texture = _texture, ShadowMapEffect = _shadowMapGenerate
-            });
-            _scene.Spawn(new ModelRenderer("foo", GraphicsDevice, _shadowMapRenderTarget) {
-                Model = _plane, Effect = _diffuse, Texture = krakula, ShadowMapEffect = _shadowMapGenerate
+            DefaultShadowMapEffect = Content.Load<Effect>("shaders/ShadowMapsGenerate");
+
+            DefaultEffect = Content.Load<Effect>("shaders/Diffuse");
+
+            _ship = new ModelRenderer("Ship", GraphicsDevice, _shadowMapRenderTarget) {
+                Model = model, Texture = texture
+            };
+            _scene.Spawn(_ship);
+            _scene.Spawn(new ModelRenderer("Plane", GraphicsDevice, _shadowMapRenderTarget) {
+                Model = plane, Texture = krakula
             });
         }
 
@@ -90,53 +77,15 @@ namespace Kineckt {
         }
 
         protected override void Draw(GameTime gameTime) {
+            // Turn camera
             var rotation = Matrix.CreateRotationY((float) gameTime.TotalGameTime.TotalSeconds);
             _scene.Camera.Position = Vector3.Transform(Vector3.Backward * 8, rotation);
-            _scene.Sun.Position = new Vector3(2, 8, 2);
+            _ship.Rotation =
+                Quaternion.CreateFromAxisAngle(Vector3.Up, (float) gameTime.TotalGameTime.TotalSeconds * -.3f);
 
-            GraphicsDevice.SetRenderTarget(_shadowMapRenderTarget);
-            GraphicsDevice.Clear(Color.Red);
-            GraphicsDevice.DepthStencilState = _depth;
-            // DrawShadowMap(_model, _scene.Sun);
-            foreach (var go in _scene.GameObjects) {
-                go.DrawShadow(_scene);
-            }
+            DrawShadows(gameTime);
 
-            GraphicsDevice.SetRenderTarget(_mainBuffer);
-            GraphicsDevice.Clear(Color.Aqua);
-            GraphicsDevice.DepthStencilState = _depth;
-
-            foreach (var go in _scene.GameObjects) {
-                go.Draw(_scene);
-            }
-            // DrawModel(_model, _scene.Camera, _scene.Sun);
-            // DrawModel(_plane, _scene.Camera, _scene.Sun);
-            /*
-            foreach (var mesh in _model.Meshes) {
-                foreach (var meshPart in mesh.MeshParts) {
-                    var modelMatrix = mesh.ParentBone.Transform;
-
-                    _diffuse.Parameters["World"].SetValue(modelMatrix);
-                    _diffuse.Parameters["View"].SetValue(viewMatrix);
-                    _diffuse.Parameters["Projection"].SetValue(projectionMatrix);
-
-                    _diffuse.Parameters["Alberto"].SetValue(_texture);
-                    _diffuse.Parameters["Shadow"].SetValue(_shadowMapRenderTarget);
-                    _diffuse.Parameters["ShadowProjection"].SetValue(modelMatrix * lightViewProjection);
-                    _diffuse.CurrentTechnique.Passes[0].Apply();
-
-                    GraphicsDevice.SetVertexBuffer(meshPart.VertexBuffer);
-                    GraphicsDevice.Indices = meshPart.IndexBuffer;
-                    var primitiveCount = meshPart.PrimitiveCount;
-                    var vertexOffset = meshPart.VertexOffset;
-                    var startIndex = meshPart.StartIndex;
-
-                    GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, vertexOffset, startIndex,
-                        primitiveCount);
-                }
-            }
-            */
-
+            DrawModels(gameTime);
 
             GraphicsDevice.SetRenderTarget(null);
             _spriteBatch.Begin(0, BlendState.Opaque, SamplerState.AnisotropicClamp);
@@ -148,54 +97,37 @@ namespace Kineckt {
             base.Draw(gameTime);
         }
 
-        /*
-        private void DrawModel(ModelRenderer modelRenderer, Camera camera, Sun sun) {
-            foreach (var mesh in modelRenderer.Meshes) {
-                foreach (var meshPart in mesh.MeshParts) {
-                    Matrix modelMatrix = mesh.ParentBone.Transform;
+        void SetupBuffers() {
+            _shadowMapRenderTarget = new RenderTarget2D(GraphicsDevice,
+                2048, 2048, false, SurfaceFormat.Single,
+                DepthFormat.Depth24, 0, RenderTargetUsage.PlatformContents);
 
-                    _diffuse.Parameters["World"].SetValue(modelMatrix);
-                    _diffuse.Parameters["View"].SetValue(camera.GetViewMatrix());
-                    _diffuse.Parameters["Projection"].SetValue(camera.GetProjectionMatrix());
+            _mainBuffer = new RenderTarget2D(GraphicsDevice, GraphicsDevice.Viewport.Width,
+                GraphicsDevice.Viewport.Height, false, SurfaceFormat.Color,
+                DepthFormat.Depth24Stencil8, 0, RenderTargetUsage.DiscardContents);
+            
+            _depth = new DepthStencilState
+                {DepthBufferEnable = true, DepthBufferFunction = CompareFunction.Less};
+        }
 
-                    _diffuse.Parameters["Alberto"].SetValue(_texture);
-                    _diffuse.Parameters["Shadow"].SetValue(_shadowMapRenderTarget);
-                    _diffuse.Parameters["ShadowProjection"].SetValue(modelMatrix * sun.GetLightViewProjection());
-                    _diffuse.CurrentTechnique.Passes[0].Apply();
+        void DrawShadows(GameTime gameTime) {
+            GraphicsDevice.SetRenderTarget(_shadowMapRenderTarget);
+            GraphicsDevice.Clear(Color.Red);
+            GraphicsDevice.DepthStencilState = _depth;
 
-                    GraphicsDevice.SetVertexBuffer(meshPart.VertexBuffer);
-                    GraphicsDevice.Indices = meshPart.IndexBuffer;
-                    var primitiveCount = meshPart.PrimitiveCount;
-                    var vertexOffset = meshPart.VertexOffset;
-                    var startIndex = meshPart.StartIndex;
-
-                    GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, vertexOffset, startIndex,
-                        primitiveCount);
-                }
+            foreach (var go in _scene.GameObjects) {
+                go.DrawShadow(_scene);
             }
         }
 
-        private void DrawShadowMap(ModelRenderer modelRenderer, Sun sun) {
-            foreach (var mesh in modelRenderer.Meshes) {
-                foreach (var meshPart in mesh.MeshParts) {
-                    Matrix modelMatrix = mesh.ParentBone.Transform;
+        void DrawModels(GameTime gameTime) {
+            GraphicsDevice.SetRenderTarget(_mainBuffer);
+            GraphicsDevice.Clear(Color.Aqua);
+            GraphicsDevice.DepthStencilState = _depth;
 
-                    _shadowMapGenerate.Parameters["LightViewProj"]
-                        .SetValue(modelMatrix * sun.GetLightViewProjection());
-
-                    _shadowMapGenerate.CurrentTechnique.Passes[0].Apply();
-
-                    GraphicsDevice.SetVertexBuffer(meshPart.VertexBuffer);
-                    GraphicsDevice.Indices = meshPart.IndexBuffer;
-                    var primitiveCount = meshPart.PrimitiveCount;
-                    var vertexOffset = meshPart.VertexOffset;
-                    var startIndex = meshPart.StartIndex;
-
-                    GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, vertexOffset, startIndex,
-                        primitiveCount);
-                }
+            foreach (var go in _scene.GameObjects) {
+                go.Draw(_scene);
             }
         }
-        */
     }
 }
--- Kineckt/src/ModelRenderer.cs
+++ Kineckt/src/ModelRenderer.cs
@@ -3,63 +3,75 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Kineckt {
     public class ModelRenderer : GameObject {
-        public Model Model { get; set; } = null;
-        public Effect Effect { get; set; } = null;
-        public Effect ShadowMapEffect { get; set; } = null;
-        public Texture Texture { get; set; } = null;
         
+        public Model Model { get; set; } = null;
+        private Effect Effect { get; set; } = Kineckt.DefaultEffect;
+        private Effect ShadowMapEffect { get; set; } = Kineckt.DefaultShadowMapEffect;
+        public Texture Texture { get; set; } = null;
+
         private readonly GraphicsDevice _graphicsDevice;
         private readonly RenderTarget2D _shadowMap;
+
         public ModelRenderer(string name, GraphicsDevice graphicsDevice, RenderTarget2D shadowMap) : base(name) {
             _graphicsDevice = graphicsDevice;
             _shadowMap = shadowMap;
         }
 
         public override void DrawShadow(Scene scene) {
-            base.DrawShadow(scene);
-            foreach (var mesh in Model.Meshes) {
-                foreach (var meshPart in mesh.MeshParts) {
-                    Matrix modelMatrix = mesh.ParentBone.Transform;
+            if (Model != null) {
+                foreach (var mesh in Model.Meshes) {
+                    foreach (var meshPart in mesh.MeshParts) {
+                        var matrix = Matrix.CreateTranslation(Position) * Matrix.CreateFromQuaternion(Rotation);
+                        var modelMatrix = mesh.ParentBone.Transform * matrix;
 
-                    ShadowMapEffect.Parameters["LightViewProj"]
-                        .SetValue(modelMatrix * scene.Sun.GetLightViewProjection());
 
-                    ShadowMapEffect.CurrentTechnique.Passes[0].Apply();
+                        ShadowMapEffect.Parameters["LightViewProj"]
+                            .SetValue(modelMatrix * scene.Sun.GetLightViewProjection());
 
-                    _graphicsDevice.SetVertexBuffer(meshPart.VertexBuffer);
-                    _graphicsDevice.Indices = meshPart.IndexBuffer;
-                    var primitiveCount = meshPart.PrimitiveCount;
-                    var vertexOffset = meshPart.VertexOffset;
-                    var startIndex = meshPart.StartIndex;
+                        ShadowMapEffect.CurrentTechnique.Passes[0].Apply();
 
-                    _graphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, vertexOffset, startIndex,
-                        primitiveCount);
+                        _graphicsDevice.SetVertexBuffer(meshPart.VertexBuffer);
+                        _graphicsDevice.Indices = meshPart.IndexBuffer;
+                        var primitiveCount = meshPart.PrimitiveCount;
+                        var vertexOffset = meshPart.VertexOffset;
+                        var startIndex = meshPart.StartIndex;
+
+                        _graphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, vertexOffset, startIndex,
+                            primitiveCount);
+                    }
                 }
             }
         }
 
         public override void Draw(Scene scene) {
-            foreach (var mesh in Model.Meshes) {
-                foreach (var meshPart in mesh.MeshParts) {
-                    Matrix modelMatrix = mesh.ParentBone.Transform;
+            if (Model != null) {
+                foreach (var mesh in Model.Meshes) {
+                    foreach (var meshPart in mesh.MeshParts) {
+                        var matrix = Matrix.CreateTranslation(Position) * Matrix.CreateFromQuaternion(Rotation);
+                        Matrix modelMatrix = mesh.ParentBone.Transform * matrix;
 
-                    Effect.Parameters["World"].SetValue(modelMatrix);
-                    Effect.Parameters["View"].SetValue(scene.Camera.GetViewMatrix());
-                    Effect.Parameters["Projection"].SetValue(scene.Camera.GetProjectionMatrix());
+                        Effect.Parameters["World"].SetValue(modelMatrix);
+                        Effect.Parameters["View"].SetValue(scene.Camera.GetViewMatrix());
+                        Effect.Parameters["Projection"].SetValue(scene.Camera.GetProjectionMatrix());
 
-                    Effect.Parameters["Alberto"].SetValue(Texture);
-                    Effect.Parameters["Shadow"].SetValue(_shadowMap);
-                    Effect.Parameters["ShadowProjection"].SetValue(modelMatrix * scene.Sun.GetLightViewProjection());
-                    Effect.CurrentTechnique.Passes[0].Apply();
+                        if (Texture != null) {
+                            Effect.Parameters["Alberto"].SetValue(Texture);
+                        }
 
-                    _graphicsDevice.SetVertexBuffer(meshPart.VertexBuffer);
-                    _graphicsDevice.Indices = meshPart.IndexBuffer;
-                    var primitiveCount = meshPart.PrimitiveCount;
-                    var vertexOffset = meshPart.VertexOffset;
-                    var startIndex = meshPart.StartIndex;
+                        Effect.Parameters["Shadow"].SetValue(_shadowMap);
+                        Effect.Parameters["ShadowProjection"]
+                            .SetValue(modelMatrix * scene.Sun.GetLightViewProjection());
+                        Effect.CurrentTechnique.Passes[0].Apply();
 
-                    _graphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, vertexOffset, startIndex,
-                        primitiveCount);
+                        _graphicsDevice.SetVertexBuffer(meshPart.VertexBuffer);
+                        _graphicsDevice.Indices = meshPart.IndexBuffer;
+                        var primitiveCount = meshPart.PrimitiveCount;
+                        var vertexOffset = meshPart.VertexOffset;
+                        var startIndex = meshPart.StartIndex;
+
+                        _graphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, vertexOffset, startIndex,
+                            primitiveCount);
+                    }
                 }
             }
         }
