59204a37e624e965c6b2e55f867367cb2756ce64
Janek Winkler <janekx21@gmail.com>
2020-04-26 15:55:24 +0200

    removed textures out of starshipOmega.fbx

--- Kineckt/src/Player.cs
+++ Kineckt/src/Player.cs
@@ -6,13 +6,15 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Kineckt {
     public class Player : ModelRenderer {
+        private static Model model;
+        private static Texture texture;
+        
         private const float Speed = 80;
         private const float Acceleration = 1200;
         private const float Friction = 600;
-        private static Model model;
-        private static Texture texture;
         private static readonly Vector3 MinPosition = new Vector3(-40, -200, -20);
         private static readonly Vector3 MaxPosition = new Vector3(40, 200, 30);
+        
         private Vector2 _vel = Vector2.Zero;
 
         public Player(GraphicsDevice graphicsDevice, RenderTarget2D shadow) : base("Player", graphicsDevice, shadow) {
@@ -45,6 +47,7 @@ namespace Kineckt {
             if (state.IsKeyDown(Keys.Down)) mov.Y += 1;
             if (mov.Length() > 1) mov.Normalize();
 
+            // makes player stop if he presses nothing
             // this is a "move towards"
             // TODO move into extension function
             if (_vel.LengthSquared() > 0) {
@@ -56,7 +59,10 @@ namespace Kineckt {
                 _vel -= c * v;
             }
 
+            // accelerate player
             _vel += mov * deltaTime * Acceleration;
+            
+            // there is a max player speed
             // this is a "clamp magnitude"
             // TODO move into extension function
             if (_vel.Length() > Speed) {
@@ -67,7 +73,9 @@ namespace Kineckt {
             var y = (float) Math.Sin(gameTime.TotalGameTime.TotalSeconds * 4.2f);
             Position += new Vector3(_vel.X, y, _vel.Y) * deltaTime;
 
+            // play area borders
             Position = Vector3.Clamp(Position, MinPosition, MaxPosition);
+            // makes player tilt in move direction
             Rotation = Quaternion.CreateFromAxisAngle(Vector3.Forward, _vel.X * .005f);
             Rotation *= Quaternion.CreateFromAxisAngle(Vector3.Right, _vel.Y * .005f);
         }
